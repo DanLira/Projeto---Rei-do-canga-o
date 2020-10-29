493564ecbf59bda5aee0f8d1c6c052d18278b7d3
Rafael Joaquim <rafaelcybersistemas@gmail.com>
2020-10-29 13:45:26 -0300

    criação do arquivo de configurações config.py instalação do flask-migrate e do flask-script e correção do arquivo __init__.py do modulo app

--- api_python/app/__init__.py
+++ api_python/app/__init__.py
@@ -1,9 +1,17 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://reidocangaco:reidocangaco@localhost/reidocangacoconexion'
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://reidocangaco:reidocangaco@localhost/reidocangacodb'
+app.config.from_object('config')
+
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+manager = Manager(app)
+manager.add_command('db', MigrateCommand)
 
 from app.controllers import default
 
---
+++ api_python/config.py
@@ -0,0 +1,5 @@
+DEBUG = True
+
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://reidocangaco:reidocangaco@localhost/reidocangacodb'
+
+SQLALCHEMY_TRACK_MODIFICATIONS = True
