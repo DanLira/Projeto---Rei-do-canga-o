5166123a589000948320022464ff6d86920a237c
Rafael Joaquim <rafaelcybersistemas@gmail.com>
2020-11-05 10:45:13 -0300

    atualizando config.py

--- api_python/config.py
+++ api_python/config.py
@@ -8,9 +8,6 @@ MYSQL_DATABASE_DB = 'reidocangacodb'
 MYSQL_DATABASE_HOST = 'localhost'
 MYSQL_DATABASE_CHARSET = 'utf8'
 
-mysqlconfig = (host = MYSQL_DATABASE_HOST, user = MYSQL_DATABASE_USER, password = MYSQL_DATABASE_PASSWORD, database = MYSQL_DATABASE_DB, charset = MYSQL_DATABASE_CHARSET)
-
-
 mysql.init_app(app)
 
 
